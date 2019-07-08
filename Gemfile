a448dfb0e221b3dfd0001588a8027a3475be7830
andmorefine <andmorefine@gmail.com>
2019-07-09 01:00:33 +0900

    commit

--- Gemfile
+++ Gemfile
@@ -3,4 +3,6 @@ source "https://rubygems.org"
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 # Specify your gem's dependencies in punctuation_marks_gem.gemspec
-gemspec
+
+gem 'rspec', '~> 3.0'
+gem 'rake', '~> 10.0'
--- lib/punctuation_marks_gem.rb
+++ lib/punctuation_marks_gem.rb
@@ -1,10 +1,10 @@
 require "punctuation_marks_gem/version"
 
 module PunctuationMarksGem
-  class Error < StandardError; end
+  # class Error < StandardError; end
   # Your code goes here...
 
   def self.greet
-    'Hello World!'
+    "Hello"
   end
 end
