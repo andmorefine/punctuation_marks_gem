f8ad76af3a6ca7bbf2fc2767123983a67f28261a
andmorefine <andmorefine@gmail.com>
2019-07-12 03:10:18 +0900

    aho

--- lib/punctuation_marks_gem.rb
+++ lib/punctuation_marks_gem.rb
@@ -3,8 +3,12 @@ require_relative "punctuation_marks_gem/version"
 module PunctuationMarksGem
   class Error < StandardError; end
   # Your code goes here...
+end
 
-  def self.greet
-    "Hello"
+class String
+  def aho
+    text_array = ["アホ", "ボケ"]
+    self.gsub!(/、/, text_array[rand(2)] + "\n")
+    self.gsub!(/。/, text_array[rand(2)] + "\n")
   end
 end
--- lib/punctuation_marks_gem/version.rb
+++ lib/punctuation_marks_gem/version.rb
@@ -1,3 +1,3 @@
 module PunctuationMarksGem
-  VERSION = "0.1.1"
+  VERSION = "0.1.2"
 end
